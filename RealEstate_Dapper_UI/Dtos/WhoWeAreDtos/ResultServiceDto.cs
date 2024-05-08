548905a73daa7922d997fb37c3d04c317817f8ad
İbrahim SARIASLAN <ibrahim.sariaslan123@hotmail.com>
2024-05-08 20:32:15 +0300

    Service Admin UI'da Listelenmesi ve SideBar'ın iconlarının eklenmesi

--- RealEstate_Dapper_UI/Dtos/WhoWeAreDtos/ResultServiceDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace RealEstate_Dapper_UI.Dtos.WhoWeAreDtos
-{
-    public class ResultServiceDto
-    {
-        public int ServiceID { get; set; }
-        public string ServiceName { get; set; }
-        public bool ServiceStatus { get; set; }
-    }
-}
