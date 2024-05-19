30116706da08066037e14fe9169bbff815ed25cf
İbrahim SARIASLAN <ibrahim.sariaslan123@hotmail.com>
2024-05-19 18:53:28 +0300

    Json Web Token Api Consume ile Giriş Yapma

--- RealEstate_Dapper_UI/Controllers/EmployeeController.cs
+++ RealEstate_Dapper_UI/Controllers/EmployeeController.cs
@@ -18,13 +18,17 @@ namespace RealEstate_Dapper_UI.Controllers
 
         public async Task<IActionResult> Index()
         {
-            var client = _httpClientFactory.CreateClient();
-            var responseMessage = await client.GetAsync("https://localhost:44353/api/Employees");
-            if (responseMessage.IsSuccessStatusCode)
-            {
-                var jsonData = await responseMessage.Content.ReadAsStringAsync();
-                var values = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
-                return View(values);
+            var token = User.Claims.FirstOrDefault(x => x.Type == "realestatetoken")?.Value;
+            if (token!= null)
+            { 
+                var client = _httpClientFactory.CreateClient();
+                var responseMessage = await client.GetAsync("https://localhost:44353/api/Employees");
+                if (responseMessage.IsSuccessStatusCode)
+                {
+                    var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                    var values = JsonConvert.DeserializeObject<List<ResultEmployeeDto>>(jsonData);
+                    return View(values);
+                }
             }
             return View();
         }
